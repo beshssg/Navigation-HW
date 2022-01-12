3fd8dcc4f48258cd2cec47bb7cb71670a9618859
beshssg <beshssg@gmail.com>
2022-01-12 16:29:59 +0500

    Fix fb

--- NavigationNET/NavigationNET/Model/LoginInspector.swift
+++ NavigationNET/NavigationNET/Model/LoginInspector.swift
@@ -8,10 +8,60 @@
 import UIKit
 import Firebase
 
-//class LoginInspector: LoginViewControllerDelegate {
-//    func checkerLogin(emailOrPhone: String, password: String) -> Bool {
-//        return Checker.shared.check(emailOrPhone: emailOrPhone, password: password)
-//    }
-//}
+class LoginInspector: LoginViewControllerDelegate {
+    
+    private(set) var login = "SnakeEyes"
+    private(set) var password = "12345"
+    
+    func loginControllerDidValidateCredentials(_ loginController: LoginViewController, completion: @escaping CredentialsVerificationCompletionBlock) {
+        guard login == self.login, !login.isEmpty else {
+            completion(.failure(CredentialsError.emptyLogin))
+            return
+        }
+        guard password == self.password, !password.isEmpty else {
+            completion(.failure(CredentialsError.emptyPassword))
+            return
+        }
+        Auth.auth().signIn(withEmail: login, password: password) { (result, error) in
+            if let error = error as NSError?, let code = AuthErrorCode(rawValue: error.code) {
+                switch code {
+                case .userNotFound:
+                    completion(.success(false))
+                    return
+                default:
+                    completion(.failure(error))
+                }
+                return
+            }
+            completion(.success(true))
+        }
+    }
+    
+    func loginControllerDidRegisterUser(_ loginController: LoginViewController, completion: @escaping CredentialsVerificationCompletionBlock) {
+        guard login == self.login, !login.isEmpty else {
+            completion(.failure(CredentialsError.emptyLogin))
+            return
+        }
+        guard password == self.password, !password.isEmpty else {
+            completion(.failure(CredentialsError.emptyPassword))
+            return
+        }
+        Auth.auth().createUser(withEmail: login, password: password) { (result, error) in
+            if let error = error as NSError?, let code = AuthErrorCode(rawValue: error.code) {
+                print(error)
+                completion(.failure(error))
+                return
+            }
+            completion(.success(true))
+        }
+    }
+    
+    
+   
+    
+    func checkerLogin(emailOrPhone: String, password: String) -> Bool {
+        return Checker.shared.check(emailOrPhone: emailOrPhone, password: password)
+    }
+}
 
 
